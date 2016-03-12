458f4023d51b4d3550c277aa377fab83bb002bf4
urpok <urpok2010@gmail.com>
2016-03-12 18:00:05 +0200

    v1.6 position center

--- js/JS.js
+++ js/JS.js
@@ -4,15 +4,22 @@ var h = window.innerHeight;
 document.getElementById("maxList").style.width = w + "px";
 document.getElementById("maxList").style.height = h + "px";
 console.log(w+" "+h);
+
 function funcInData() {
 var ex = document.getElementById("innerData").value;
-var pic = document.createElement('img');
-pic.src = ex;
-list.appendChild(pic);
-pic.className = 'smallImg';
-var wideCount = counter*308;
-counter++;
-document.getElementById("list").style.width = wideCount + "px";
-//document.getElementById("maxList").style.width = w + "px";
+var wideCount = counter*310;
+
+//document.getElementById("maxList").style.maxWidth = w + "px";
 //document.getElementById("maxList").style.height = h + "px";
+
+if (wideCount<w){
+	document.getElementById("list").style.width = wideCount + "px";
+	var pic = document.createElement('img');
+	pic.src = ex;
+	list.appendChild(pic);
+	pic.className = 'smallImg';
+	counter++;
+} else {
+	document.getElementById("list").style.height = 610 + "px";
+}
 };
