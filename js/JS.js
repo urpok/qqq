e88ba37bd770e7315dcd222640f2b503fa2ecec1
urpok <urpok2010@gmail.com>
2016-03-12 02:50:54 +0200

    v1.3

--- js/JS.js
+++ js/JS.js
@@ -1,7 +1,14 @@
+var counter=1;
+var w = window.innerWidth;
+var h = window.innerHeight;
+
 function funcInData() {
 var ex = document.getElementById("innerData").value;
 var pic = document.createElement('img');
 pic.src = ex;
 list.appendChild(pic);
-pic.className('smallImg');
+pic.className = 'smallImg';
+var wideCount = counter*308;
+counter++;
+document.getElementById("list").style.width = wideCount + "px";
 };
